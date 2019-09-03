623d979e6e5c4109fc17c8f9e6b3b364b898f7e6
Jason Olson <jolson88@outlook.com>
2019-09-02 23:02:49 -0700

    Parses 'load address' opcode

--- src/opcode.rs
+++ src/opcode.rs
@@ -1,19 +1,28 @@
 use crate::cpu::{register_from_u8, Register};
 
+/// Represents different opcodes that the Chip-8 can execute.
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub enum Opcode {
+    /// The address to load from into I
+    LoadAddress(u16),
+    /// The constant value to load into the Register
     LoadConstant(Register, u8),
 }
 
-pub fn opcode_from_binary(hi_byte: u8, lo_byte: u8) -> Opcode {
+/// Converts a u16 into an Opcode. Takes a u16 as all Chip-8 instructions are 2-bytes.
+pub fn opcode_from_instruction(inst: u16) -> Opcode {
     // We can use the top 4-bits of the opcode as a switch into the type of opcode for easier parsing
-    let opcode_switch = hi_byte >> 4;
+    let opcode_switch = inst >> 12;
     match opcode_switch {
-        6 => {
-            Opcode::LoadConstant(register_from_u8(hi_byte & 0x0F), lo_byte)
+        0x6 => {
+            Opcode::LoadConstant(register_from_u8((inst >> 8) as u8 & 0x0F), (inst & 0xFF) as u8)
         },
+        0xA => {
+            // The address to load from is the bottom 12-bits
+            Opcode::LoadAddress(inst & 0xFFF)
+        }
         _ => {
-            unimplemented!("Opcode not recognized: 0x{:X}{:X}", hi_byte, lo_byte);
+            unimplemented!("Instruction not recognized: {:X}", inst);
         }
     }
 }
@@ -23,8 +32,9 @@ mod tests {
     use super::*;
 
     #[test]
-    fn parses_opcodes() {
-        assert_eq!(Opcode::LoadConstant(Register::VA, 0x02), opcode_from_binary(0x6A, 0x02));
-        assert_eq!(Opcode::LoadConstant(Register::V0, 0xFF), opcode_from_binary(0x60, 0xFF));
+    fn parses_load_opcodes() {
+        assert_eq!(Opcode::LoadConstant(Register::VA, 0x02), opcode_from_instruction(0x6A02));
+        assert_eq!(Opcode::LoadConstant(Register::V0, 0xFF), opcode_from_instruction(0x60FF));
+        assert_eq!(Opcode::LoadAddress(0x2EA), opcode_from_instruction(0xA2EA));
     }
 }
